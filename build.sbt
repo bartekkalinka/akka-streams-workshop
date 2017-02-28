58a9ffd8c48b22070abf041475371cce6c485489
bartlomiej.kalinka <bartkali@hotmail.com>
2017-02-28 16:25:27 +0100

    1.1

--- build.sbt
+++ build.sbt
@@ -4,4 +4,9 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
-    
+libraryDependencies ++= {
+  val akkaV       = "2.4.17"
+  Seq(
+    "com.typesafe.akka" %% "akka-stream" % akkaV
+  )
+}
--- src/main/scala/edu/Main.scala
+++ src/main/scala/edu/Main.scala
@@ -1,5 +1,25 @@
 package edu
 
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.{RunnableGraph, Sink, Source}
+
+//introduction
+case class Lesson1(implicit val system: ActorSystem, materializer: ActorMaterializer) {
+
+  //first stream
+	def example1 = {
+    val stream: RunnableGraph[NotUsed] = Source(List(1, 2, 3)).to(Sink.foreach(println))
+    val notUsed: NotUsed = stream.run
+  }
+}
+
 object Main extends App {
-	println("hello world")
+  implicit val system = ActorSystem("Lesson1")
+  implicit val materializer = ActorMaterializer()
+
+  Lesson1().example1
+
+  system.terminate()
 }
